cfc55ffea177f3dc023b27425f0d50a53baa2f81
Kae Ren <hkr0528@gmail.com>
2022-06-12 21:40:32 +0800

    add pickle

--- app.py
+++ app.py
@@ -20,14 +20,14 @@ st.markdown("###### Group: Mobile Legend")
 st.markdown("---")
 
 st.markdown(
-    "<div style='text-align: justify'> Depression <b>major depressive disorder</b> is a common and serious medical illness that negatively affects how you feel, the way you think and how you act. It can lead to a variety of emotional and physical problems and can decrease your ability to function at work and at home. So, we build a text classification machine learning model which can predict depression level through text </div>", unsafe_allow_html=True
+    "<div style='text-align: justify'> Depression <b>(major depressive disorder)</b> is a common and serious medical illness that negatively affects how you feel, the way you think and how you act. It can lead to a variety of emotional and physical problems and can decrease your ability to function at work and at home. So, we build a text classification machine learning model which can predict depression level through text.</div>", unsafe_allow_html=True
 )
 
 st.markdown('')
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    new_model = tf.keras.models.load_model('model_depressed.h5')
+    new_model = tf.keras.models.load_model('model_depressed_2.h5')
     return new_model
 
 # loading
@@ -50,34 +50,35 @@ with st.form(key = 'form_1'):
     padded_sentence = sentence_process(arr)
     results = new_model.predict(padded_sentence)
     if results[0,0] > 0.75:
+        results_2 = "Severe depression"
+        advice = "Counseling for depression: Since your depression symptoms are very serious, please seek for the help of a professional counsellor. So, you will explore why depression has occurred and look for ways to overcome it."
+        st.error("Severe depression detected")
+    elif 0.5 <= results[0,0] <= 0.75:
+        results_2 = "Moderate depression"
+        advice = "Talking therapy: Please have a try on talking therapy to destress yourself. Talking therapy is a series of sessions, the individual will work with a counselor to identify the causes of depression and find ways of resolving it."
+        st.warning("Moderate depression detected")
+    elif 0.25 <= results[0,0] <= 0.5:
+        results_2 = "Mild depression"
+        advice = "Guided self-help: Since your depression symptoms are not too serious, you may follow an online course or manual with the support of a therapist. The course aims to provide tools that enable a person to make helpful changes."
+        st.info("Mild depression detected")
+    elif 0.001 <= results[0,0] <= 0.25:
         results_2 = "No depression"
         advice = "you are totally fine"
-        st.success("You are happy")
-    elif 0.5 <= results[0,0] <= 0.75:
-        results_2 = "Mild depression"
-        advice = "Guided self-help: The person may follow an online course or manual with the support of a therapist. The course aims to provide tools that enable a person to make helpful changes"
-        st.info("Mild depression")
-    elif 0.25 <= results[0,0] <= 0.5:
-        results_2 = "Moderate depression"
-        advice = "Talking therapy: During a series of sessions, the individual will work with a counselor to identify the causes of depression and find ways of resolving it"
-        st.warning("Moderate Depression")
-    elif 0.001 <= results[0,0] <= 0.25:
-        results_2 = "Severe depression"
-        advice = "Counseling for depression: The person will explore why depression has occurred and look for ways to overcome it."
-        st.error("Severe depression")
+        st.success("No depression detected")
     else:
         results_2 =""
+        advice = ""
 
 st.markdown("## Prediction Result")
 st.markdown(f"Predictions : {results_2}")
 my_bar = st.progress(0)
 
 for percent_complete in range(int(results[0,0] * 100)):
-    time.sleep(0.01)
+    time.sleep(0.5)
     my_bar.progress(percent_complete + 1)
 
 resultPercentage = "{:.2f}".format(results[0,0] * 100)
-st.markdown(f"The Probability of not being depressed : {resultPercentage} %")
+st.markdown(f"The Probability of being depressed : {resultPercentage} %")
 st.markdown("## Advice for " + f"{results_2}")
 st.info(advice)
 
